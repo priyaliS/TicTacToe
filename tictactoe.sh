3a82cbbf42ce00d94ec062f241b0863ea603442b
priyalisakhare31@gmail.com <priyalisakhare31@gmail.com>
2020-07-08 19:11:17 +0530

    [Priyali]Added: Assigned symbols X or 0

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,15 +1,31 @@
-#! /bin/bash/ 
+#! /bin/bash 
 
 echo "TicTakToe game"
 
-array=(@ @ @ @ @ @ @ @ @ ) 
+array=(@ @ @ @ @ @ @ @ @)
+
+player=1
 
 function playingBox()
 {
 
-	echo "0  ${array[0]} ${array[1]} ${array[2]}"
-	echo "1  ${array[3]} ${array[4]} ${array[5]}"
-	echo "2  ${array[6]} ${array[7]} ${array[8]}"
+	echo "row 0  ${array[0]} ${array[1]} ${array[2]}"
+	echo "row 1  ${array[3]} ${array[4]} ${array[5]}"
+	echo "row 2  ${array[6]} ${array[7]} ${array[8]}"
 }
 
 playingBox
+
+function symbols()
+{
+
+	if [ $(( $RANDOM%2 )) -eq 1 ]
+	then 
+		player_Symbol=X
+	else
+		player_Symbol=O
+	fi
+
+	echo "player symbol is : "$player_Symbol
+}
+symbols
