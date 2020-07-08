4aac144131655974fdc016d96cc2a3cb32dea73c
priyalisakhare31@gmail.com <priyalisakhare31@gmail.com>
2020-07-08 19:25:16 +0530

    [Priyali]Added : Toss to check who will play first

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,31 +1,53 @@
-#! /bin/bash 
+#! /bin/bash
 
 echo "TicTakToe game"
 
 array=(@ @ @ @ @ @ @ @ @)
-
 player=1
 
 function playingBox()
 {
 
-	echo "row 0  ${array[0]} ${array[1]} ${array[2]}"
-	echo "row 1  ${array[3]} ${array[4]} ${array[5]}"
-	echo "row 2  ${array[6]} ${array[7]} ${array[8]}"
+	echo "row 0   ${array[0]} ${array[1]} ${array[2]}"
+	echo "row 1   ${array[3]} ${array[4]} ${array[5]}"
+	echo "row 2   ${array[6]} ${array[7]} ${array[8]}"
 }
 
 playingBox
 
+function toss()
+{
+
+echo "----option are----"
+echo "1.Head"
+echo "2.Tail"
+
+	read -r tossOption
+
+	tossNum=$(( $RANDOM%2 +1 ))
+
+	if [ $tossNum -eq $tossOption ]
+	then
+		echo "player won toss"
+	else
+		echo "player lost toss"
+		player=$(( (( $player%2 ))+1 ))
+	fi
+}
+
 function symbols()
 {
 
-	if [ $(( $RANDOM%2 )) -eq 1 ]
-	then 
-		player_Symbol=X
-	else
-		player_Symbol=O
-	fi
+        if [ $(( $RANDOM%2 )) -eq 1 ]
+        then
+                playerSymbol=X
+        else
+                playerSymbol=O
+        fi
 
-	echo "player symbol is : "$player_Symbol
+        echo "player choosed" $playerSymbol
+
+        toss
 }
 symbols
+
